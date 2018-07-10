39298642a0981799ea0f4579458b88d74bd4d4e7
atomita <sleeping.cait.sith+gh@gmail.com>
2018-07-10 16:27:39 +0900

    Add rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/xatto-admin-lte.js',
+    format: 'umd',
+    name: 'xatto-admin-lte',
+    sourceMap: true,
+  },
+  plugins: [typescript({
+    exclude: [
+      '*.d.ts',
+      '**/*.d.ts',
+      '*.test.ts',
+      '**/*.test.ts',
+      '*.test.tsx',
+      '**/*.test.tsx'
+    ]
+  })]
+}
